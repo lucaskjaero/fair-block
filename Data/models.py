5cecbe68480a8b609db37b252bc06a3d40726a09
Lucas Kjaero <lucas@lucaskjaero.com>
2018-03-03 15:35:50 -0800

    First run

--- Data/models.py
+++ Data/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class ParkingSpot(models.Model):
     latitude = models.CharField(max_length=20)
     longitude = models.CharField(max_length=20)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, blank=True)
     uuid = models.CharField(max_length=50, primary_key=True)
 
 class State(models.Model):
