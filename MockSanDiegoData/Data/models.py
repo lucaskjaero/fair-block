f4c322a1bcf022bfe0e050e01b3096c70326a99b
Lucas Kjaero <lucas@lucaskjaero.com>
2018-03-03 12:32:32 -0800

    Basic runtime

--- MockSanDiegoData/Data/models.py
+++ MockSanDiegoData/Data/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ParkingSpot(models.Model):
+    latitude = models.CharField(max_length=10)
+    longitude = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+
+class State(models.Model):
+    timestamp = models.DateTimeField()
+    occupied = models.BooleanField()
+    parking_spot = models.ForeignKey(ParkingSpot, on_delete=models.CASCADE)
