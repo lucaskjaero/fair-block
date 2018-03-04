c03caca6b288bb1d46553a2ec8509c0763479fea
Lucas Kjaero <lucas@lucaskjaero.com>
2018-03-03 20:59:29 -0800

    Price and hours

--- Data/models.py
+++ Data/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
 class ParkingSpot(models.Model):
     latitude = models.CharField(max_length=20)
     longitude = models.CharField(max_length=20)
+
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    max_hours = models.IntegerField()
+
     name = models.CharField(max_length=50, blank=True)
     uuid = models.CharField(max_length=50, primary_key=True)
 
+
 class State(models.Model):
     time_in = models.DateTimeField()
-    time_out = models.DateTimeField()
+    time_out = models.DateTimeField(blank=True)
     occupied_by = models.CharField(max_length=200)
     parking_spot = models.ForeignKey(ParkingSpot, on_delete=models.CASCADE)
